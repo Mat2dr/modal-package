152df8129e36c67b1a4891e93d84263b146157a9
Mat2dr <dragovic.mathias@gmail.com>
2023-03-27 16:48:37 +0200

    Updated readme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./Modal.css";
 import { CSSTransition } from 'react-transition-group'
 
+/**
+ * @name Modal
+ * @description the modal component
+ * @return {JSX.Element}} 
+ */
 export const Modal = props => {
 
     const closeOnEscapeKeyDown = e => {
